e7cde4b2fa6bdd756c2410c4bbd0322f7da0a1f4
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-05-15 14:51:55 -0300

    Creando Rutas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserTouter, Routes ,Route } from "react-router-dom"
+import { BrowserTouter, Routes ,Route, BrowserRouter } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes><Route exacr path='/' element={<Login/>}/></Routes>
+      </BrowserRouter>
     </div>
   );
 }
