f74aeadb4dcdd5a7eddb12c22a0859bf20882cde
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-05-15 15:05:00 -0300

    Importando Login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes ,Route, } from "react-router-dom"
-
+import { Login } from './components/login/login.jsx'
 function App() {
   return (
     <div className="App">
