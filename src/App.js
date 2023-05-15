8654c9f51964a9f6dec2bb888e6669d25e8c51c0
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-05-15 14:53:05 -0300

    Modificando import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserTouter, Routes ,Route, BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes ,Route, } from "react-router-dom"
 
 function App() {
   return (
