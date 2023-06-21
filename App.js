51eee523e94194c3cdb76256c23bfad457ca97e2
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-06-21 03:18:13 -0300

    Se modifico App.js

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { NativeRouter, Route } from 'react-router-native';
-import Login from './components/login/Login.jsx';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => {
+export default function App() {
   return (
-    <NativeRouter>
-      <Route exact path="/" component={Login} />
-    </NativeRouter>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
