8d515029ddd8112d01f53fbfc2f6a3406c33c769
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-06-22 17:31:46 -0300

    Eliminando contenido de view

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import Constants from 'expo-constants'; //importo para utilizar el margin predefinido para no colicinar con la barra superior
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={ {marginTop: Constants.statusBarHeight/* Margir predefinido */, flexGrow: 1 }}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ {marginTop: Constants.statusBarHeight/* Marginpredefinido */, flexGrow: 1 }}>
+      
     </View>
   );
 }
