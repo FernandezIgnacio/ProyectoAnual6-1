6d637fa510c4422f0fcf1a677706f8ac56c0fd9c
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-06-22 18:27:27 -0300

    Agregando el login

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Constants from 'expo-constants'; //importo para utilizar el margin predefinido para no colicinar con la barra superior
 import { View ,Text } from 'react-native';
+import Login from "./src/components/Login";
 
 export default function App() {
   return (
-    <View style={ {marginTop: Constants.statusBarHeight/* Marginpredefinido */, flexGrow: 1 }}>
-      <Text>hola</Text>
+    <View style={ {marginTop: Constants.statusBarHeight/* Margin predefinido */, flexGrow: 1 }}>
+      <Login />
     </View>
   );
 }
