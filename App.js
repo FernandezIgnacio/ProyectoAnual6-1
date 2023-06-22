432d0a8b1c651ea624853f73a1a31a292a3d3979
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-06-22 17:01:01 -0300

    Agregando margin superior predefinido por expo

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import Constants from 'expo-constants'
+import Constants from 'expo-constants'; //importo para utilizar el margin predefinido para no colicinar con la barra superior
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={ {marginTop: Constants.statusBarHeight/* Margir predefinido */, flexGrow: 1 }}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
