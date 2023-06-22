b36d28ba1757efc0d474000b873234b04f7a24e4
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-06-22 17:52:07 -0300

    Se modifico App.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
+import React from "react";
 import Constants from 'expo-constants'; //importo para utilizar el margin predefinido para no colicinar con la barra superior
-import { StyleSheet, Text, View } from 'react-native';
+import { View ,Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={ {marginTop: Constants.statusBarHeight/* Marginpredefinido */, flexGrow: 1 }}>
-      
+      <Text>hola</Text>
     </View>
   );
 }
