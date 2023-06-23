61732af2df6ef854fef3dfba0227da4b2af53611
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-06-22 21:00:58 -0300

    Se agrego firebase al archivo App.js

--- App.js
+++ App.js
@@ -3,10 +3,14 @@ import Constants from 'expo-constants'; //importo para utilizar el margin predef
 import { View ,Text } from 'react-native';
 import Login from "./src/components/Login";
 
+import { app } from './firebase'; // Ruta correcta al archivo firebase.js
+
+
 export default function App() {
   return (
     <View style={ {marginTop: Constants.statusBarHeight, flexGrow: 1 }}>
       <Login />
+      
     </View>
   );
 }
