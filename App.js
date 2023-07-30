cfc46c8888e9028fe0dfeb4c522a1e47f1d111f0
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-07-29 23:45:45 -0300

    Se modificaron los comentarios

--- App.js
+++ App.js
@@ -1,3 +1,32 @@
+import React from "react";
+import Constants from 'expo-constants'; //importo para utilizar el margin predefinido para no colicinar con la barra superior
+import { View} from 'react-native'
+import { Route } from "react-router-native";
+
+import Login from "./src/components/Login.jsx";
+
+import { app } from './firebase'; // Ruta correcta al archivo firebase.js
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const auth = getAuth(app);
+
+const checkFirebaseConnection = () => {
+  onAuthStateChanged(auth, (user) => {
+    console.log(user);
+  });
+};
+ 
+checkFirebaseConnection();
+
+export default function App() {
+  return (
+    <View style={ {marginTop: Constants.statusBarHeight, flexGrow: 1,justifyContent: 'center', textAlign: 'center'}}>
+      <Login />  
+    </View>
+    
+  );
+}
+
 // import React, { useState } from 'react';
 // import Constants from 'expo-constants'; //importo para utilizar el margin predefinido para no colicinar con la barra superior
 // import { View, TextInput, Button } from 'react-native';
@@ -33,34 +62,6 @@
 //   );
 // };
 
-import React from "react";
-import Constants from 'expo-constants'; //importo para utilizar el margin predefinido para no colicinar con la barra superior
-import { View } from 'react-native';
-import { NativeRouter } from "react-router-native";
-
-import Login from "./src/components/Login";
-
-import { app } from './firebase'; // Ruta correcta al archivo firebase.js
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-
-const auth = getAuth(app);
-
-const checkFirebaseConnection = () => {
-  onAuthStateChanged(auth, (user) => {
-    console.log(user);
-  });
-};
-
-checkFirebaseConnection();
-
-export default function App() {
-  return (
-    <NativeRouter style={ {marginTop: Constants.statusBarHeight, flexGrow: 1,justifyContent: 'center', textAlign: 'center'}}>
-      <Login />
-    </NativeRouter>
-    
-  );
-}
 
 /* ESTA ES LA FUNCIONAEL PARA PROBAR */
 
