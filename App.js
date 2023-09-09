f18c156a40c5b3a528643ea04cf9a431c883ab77
kameldayoub <kamel.dayoub.epet20@gmail.com>
2023-09-09 19:49:10 -0300

    Cambiando ubicacion de funcion

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "./src/components/Login2"
+import Login from "./src/components/Login"
 
 const Stack = createNativeStackNavigator();
 
