dad6c67396aba13befc59916267c432811d5a561
Kamel Dayoub <kamel.dayoub.epet20@gmail.com>
2023-11-13 11:36:40 -0300

    Se importo PropTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Home from "./components/home";
-import Login from "./components/login";
+import PropTypes from 'prop-types'; // Import prop-types module
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/home';
+import Login from './components/login';
 
 function App() {
   const currentUser = false;
 
-  const RequireAuth =({children}) => {
-    return currentUser ? (children) : <Navigate to="/login" />
-  }
+  const RequireAuth = ({ children }) => {
+    return currentUser ? children : <Navigate to="/login" />;
+  };
+
+  // Add prop validation for RequireAuth component
+  RequireAuth.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
 
   return (
     <div>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route path="login" element={<Login/>}/>
-            <Route index element={
-              <RequireAuth>
-                <Home/>
-              </RequireAuth>}
+            <Route path="login" element={<Login />} />
+            <Route
+              index
+              element={
+                <RequireAuth>
+                  <Home />
+                </RequireAuth>
+              }
             />
           </Route>
         </Routes>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
