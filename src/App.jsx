cf8be3d22e2220beba667143f4074df4768d6502
Kamel Dayoub <kamel.dayoub.epet20@gmail.com>
2023-11-13 11:18:35 -0300

    Creando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/home";
+import Login from "./components/login";
+
 function App() {
+  const currentUser = false;
+
+  const RequireAuth =({children}) => {
+    return currentUser ? (children) : <Navigate to="/login" />
+  }
 
   return (
-    <>
-      <div>
-        hola
-      </div>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route path="login" element={<Login/>}/>
+            <Route index element={
+              <RequireAuth>
+                <Home/>
+              </RequireAuth>}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
